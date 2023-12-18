99450121940a9b321c6c07ac1b5bdbd80392a633
goyorr <zakariago9@gmail.com>
2023-12-18 03:27:30 -0500

    ???

--- src/requirements/wordpress/tools/start.sh
+++ src/requirements/wordpress/tools/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir -p /wp-isntall
+mkdir -p /wp-install
 
 chmod -R 777 /wp-install;
 
@@ -17,12 +17,15 @@ wp config set DB_PASSWORD $DB_ROOT_PASSWD --allow-root
 
 wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_EMAIL --skip-email --allow-root
 
+
 wp user create $WP_USER $WP_EMAIL_USER --role=author --user_pass=$WP_USER_PASSWD --allow-root
 
 wp plugin update --all --allow-root
 
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
-chown -R www-data:www-data /wp
+chown -R www-data:www-data /wp-install
 
 /usr/sbin/php-fpm7.4 -F
+ 
+ 
